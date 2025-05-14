a234579c464833d447c7b648585a1fbca2c9a8a6
shubhamB300 <shubhambirje300@gmail.com>
2025-05-14 13:49:37 +0530

    Initial commit: Added goFood MERN stack project files

---
+++ goFood/backend/db.js
@@ -0,0 +1,35 @@
+require('dotenv').config();  // Load environment variables from .env
+
+const mongoose = require('mongoose');
+//const app = express();
+//app.use(express.json());
+
+const mongoURI= process.env.MONGO_URI;
+
+const mongoDB = async()=>{
+    await mongoose.connect(mongoURI,{ useNewUrlParser: true },async(err,result)=>{
+        if(err) console.log("---", err);
+        else{ 
+            console.log('connected'); 
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray( async function( err, data) {
+                const foodCategory = await mongoose.connection.db.collection("foodCategory");
+                foodCategory.find({}).toArray(function(err, catData){
+                    if(err) console.log(err);
+                    else {
+                        global.food_items = data;
+                        global.foodCategory = catData;
+                    }
+                })
+                // if(err) console.log(err);
+                // else {
+                //     global.food_items = data;
+                //     console.log()
+                // }
+            })
+        }
+
+    });        
+}
+
+module.exports = mongoDB;
